922265f65fac3b9c749f4ed8471228163cdc1f71
Ishan Shrestha <ishan.shrestha@laliguras.de>
2023-09-06 15:27:46 +0545

    Updated config file

--- controllers/openai_controller.py
+++ controllers/openai_controller.py
@@ -1,14 +1,17 @@
 import os
 import openai
 from dotenv import load_dotenv
-
+from config import Config
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+CONFIG = Config.openAI_config()
+openai.api_key = CONFIG['API_KEY']
 if openai.api_key is None:
     raise ValueError("OPENAI_API_KEY environment variable not found. Please set it in the .env file.")
 
 def get_openai_response(engine: str, prompt: str) -> str:
+    print(f"here is confgi {CONFIG} & {CONFIG['API_KEY']}")
+
     try:
         response = openai.Completion.create(engine=engine, prompt=prompt, max_tokens=2049, temperature=0.7, n=1, stop=None)
         return response.choices[0].text.strip()
